42caef28bfdda57855fe6a8a8784829dc495accf
V1cente <vmabarros@gmail.com>
2022-05-23 12:43:41 +0100

    REMOVE: Useless images

--- src/data/homeData.js
+++ src/data/homeData.js
@@ -1,23 +1,23 @@
 export const homeData = [
             {
-                "name": "Informação Privilegiada",
-                "image": "https://popcasts.pt/wp-content/uploads/2021/09/image-1628157425-scaled.jpg"
+                name: "Informação Privilegiada",
+                image: "https://popcasts.pt/wp-content/uploads/2021/09/image-1628157425-scaled.jpg"
             },
             {
-                "name": "Extremamente Desagradável",
-                "image": "https://i.scdn.co/image/ab6765630000ba8a3c9f36d771c507a16572ffa3"
+                name: "Extremamente Desagradável",
+                image: "https://i.scdn.co/image/ab6765630000ba8a3c9f36d771c507a16572ffa3"
             },  
             {
-                "name": "Informação Privilegiada",
-                "image": "https://popcasts.pt/wp-content/uploads/2021/09/image-1628157425-scaled.jpg"
+                name: "Informação Privilegiada",
+                image: "https://popcasts.pt/wp-content/uploads/2021/09/image-1628157425-scaled.jpg"
             },
             {
-                "name": "Extremamente Desagradável",
-                "image": "https://i.scdn.co/image/ab6765630000ba8a3c9f36d771c507a16572ffa3"
+                name: "Extremamente Desagradável",
+                image: "https://i.scdn.co/image/ab6765630000ba8a3c9f36d771c507a16572ffa3"
             },
             {
-                "name": "Extremamente Desagradável",
-                "image": "https://i.scdn.co/image/ab6765630000ba8a3c9f36d771c507a16572ffa3"
+                name: "Extremamente Desagradável",
+                image: "https://i.scdn.co/image/ab6765630000ba8a3c9f36d771c507a16572ffa3"
             },            
              
 ]
