8041e8334c15f8c5e18439f67349be76cd7a81ab
V1cente <vmabarros@gmail.com>
2022-05-28 15:04:11 +0100

    NEW: fullData.js

---
+++ src/data/fullData.js
@@ -0,0 +1,38 @@
+export const fullData =[
+        {
+            id: 1,
+            name: "Rádio Comercial",
+            icon: "https://cdn6.aptoide.com/imgs/e/c/d/ecda4b53f0e126b3f39edc6d7036328f_icon.png?w=128",
+            banner : "",
+            liveNow: "Que barulho é este?",
+            programs: [
+                {
+                    id: 1,
+                    name: "Extremamente Desagradável",
+                    description: "A rubrica de humor de “As Três da Manhã” está a cargo de Joana Marques, que se esforça sempre por ser “extremamente desagradável”, agora na Renascença. De segunda a sexta, às 8h15 com repetição às 16h20, 20h30 e às 6h45.",
+                    icon : "https://i.scdn.co/image/ab6765630000ba8a3c9f36d771c507a16572ffa3",
+                    banner : "https://gruporenascencamultimedia.com/wp-content/uploads/2021/12/extre-desagradavel_base-002.jpg",
+                    genres : [
+                        "Comedy",
+                        "Pop",
+                    ],
+                    episodes: [
+                        {
+                            id: 1,
+                            name: "Rúben Semedo é Alerta CM",
+                            description: "Joana Marques fala-nos da mais recente polémica envolvendo Ruben Semedo.",
+                            date: "22/04/2022",
+                            time: "14"
+                        },
+                        {
+                            id: 2,
+                            name: "O Último a Trair",
+                            description: "Joana Marques fala-nos da atribulada lua de mel de Tiago e Dina, participantes do \"Casados à primeira vista\".",
+                            date: "21/04/2022",
+                            time: "15"
+                        }
+                    ]
+                }
+            ]   
+        }
+]
