454e8fbcc8b15f1d0ecdc8b40fe4574db68e4701
V1cente <vmabarros@gmail.com>
2022-05-31 16:34:08 +0100

    UPDATE: Homepage

---
+++ src/data/homeData.js
@@ -0,0 +1,94 @@
+export const homeData = [
+    {
+        id: 0,
+        title: "For You",
+        content: [
+            {
+                id:0,
+                name: "Geração Z",
+                image: "https://cdn.popcasts.pt/wp-content/uploads/2022/04/image-1648122234-2048x2048.jpg",
+                path: "/radio/4/program/2"
+            },
+            {
+                id: 1,
+                name: "Se fosse a ti não atendia!!",
+                image: "https://cdn.popcasts.pt/wp-content/uploads/2022/05/image-1652892426-2048x2048.jpg",
+                path: "/radio/3/program/0"
+            },
+            {
+                id: 2,
+                name: "Cala-te Boca!",
+                image: "https://cdn.popcasts.pt/wp-content/uploads/2021/09/image-1627634995-2048x2048.jpg",
+                path: "/radio/3/program/1"
+            },
+            {
+                id: 3,
+                name: "Hotel Califórnia",
+                image: "https://cdn.popcasts.pt/wp-content/uploads/2022/05/image-1652110820-2048x2048.jpg",
+                path: "/radio/4/program/0"
+            }
+        ]               
+    },
+
+
+    {
+        id: 1,
+        title: "🔥 Now",
+        content: [
+            {
+                id:0,
+                name: "Rádio Comercial",
+                image: "http://radiocomercial.iol.pt/images/noCover.png",
+                path: "/radio/0"
+            },
+            {
+                id: 1,
+                name: "Cala-te Boca!",
+                image: "https://cdn.popcasts.pt/wp-content/uploads/2021/09/image-1627634995-2048x2048.jpg",
+                path: "/radio/3/program/1"
+            },
+            {
+                id: 2,
+                name: "STAND-UP NA HORA",
+                image: "https://cdn.popcasts.pt/wp-content/uploads/2021/09/image-1628158532-2048x2048.jpg",
+                path: "/radio/1/program/0"
+            },
+            {
+                id: 3,
+                name: "Futuro da agricultura e alterações climáticas. “Estamos a perder área de cultivo”",
+                image: "https://cdn.popcasts.pt/wp-content/uploads/2022/04/image-1648122275-2048x2048.jpg",
+                path: "/radio/1/program/0"
+            }
+        ]               
+    },
+    {
+        id: 2,
+        title: "Best Radios",
+        content: [
+            {
+                id: 0,
+                name: "RFM",
+                image: "https://play-lh.googleusercontent.com/MhpmqORmqFYvYLYKV2hWwcKz9Ned_l1Mjznbm0G5YVETlXfa0rj3gnjPP30ZtV3isSg",
+                path: "/radio/1"
+            },
+            {
+                id: 1,
+                name: "CidadeFM",
+                image: "https://cidade.iol.pt/images/noCover.png",
+                path: "/radio/2"
+            },
+            {
+                id:2,
+                name: "Rádio Comercial",
+                image: "http://radiocomercial.iol.pt/images/noCover.png",
+                path: "/radio/0"
+            },
+            {
+                id: 3,
+                name: "MegaHits",
+                image: "https://cdn.popcasts.pt/wp-content/uploads/2021/04/MEGA-HITS.jpg",
+                path: "/radio/3"
+            }
+        ]               
+    }
+]
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,16 @@
 import Sidebar from "../components/Sidebar"
 import Player from "../components/Player"
 import ContentBar from "../components/Contentbar"
+import { homeData } from "../data/homeData"
 const Home = () => {
     return (
         <div className="flex h-screen">
             <Sidebar></Sidebar>
             <div className="h-screen w-screen overflow-y-hidden flex flex-col">
-                <div className="mx-auto mt-3 overflow-y-auto scrollbar-hide mb-auto">
-                    <h1>NOT WORKING</h1>
-                    {/*<ContentBar title={"For You"} content={homeData}/>
-                    <ContentBar title={"Your Radios"} content={homeData}/>
-                    <ContentBar title={"Keep Watching"} content={homeData}/>*/}
-
+                <div className="mx-auto mt-3 overflow-y-auto scrollbar-hide mb-auto pb-4">
+                    <ContentBar title={homeData[0].title} content={homeData[0].content}/>
+                    <ContentBar title={homeData[1].title} content={homeData[1].content}/>
+                    <ContentBar title={homeData[2].title} content={homeData[2].content}/>
                 </div>
                 <Player></Player>
             </div>
